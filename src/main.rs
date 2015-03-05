da3b26f355b5dc9099d3215e1c2e4feb8bc17256
J/A <archer884@gmail.com>
2015-03-05 15:42:02 -0600

    Converted to f64 just to get done

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,35 @@
-extern crate num;
-
-use num::bigint::BigInt;
-use num::rational::Ratio;
-
-type ORatio = Option<Ratio<BigInt>>;
+type OFloat = Option<f64>;
 
 struct TipResult {
-    amt: Ratio<BigInt>,
-    tip: Ratio<BigInt>,
+    amt: f64,
+    tip: f64,
 }
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
 
     let tip = match &args[..] {
-        [_, ref amt] => get_tip(ratio(&amt), Ratio::from_float(0.15f32)),
-        [_, ref amt, ref pct] => get_tip(ratio(&amt), ratio(&pct)),
+        [_, ref amt] => get_tip(amt.parse().ok(), Some(0.15f64)),
+        [_, ref amt, ref pct] => get_tip(amt.parse().ok(), pct.parse().ok()),
         _ => None,
     };
 
     match tip {
-        Some(tip) => println!("${:.2} on ${:.2}", tip.tip, tip.amt.numer() / tip.amt.denom()),
-        None => println!("USAGE: {} AMOUNT <decimal> [TIP PERCENT] <decimal>", args[0]),
+        Some(tip) => println!("${:.2} on ${:.2}", tip.tip, tip.amt),
+        None => println!("USAGE: {} AMOUNT <decimal> [TIP PERCENT] <integer>", args[0]),
     }
 }
 
-fn get_tip(amt: ORatio, pct: ORatio) -> Option<TipResult> {
+fn get_tip(amt: OFloat, pct: OFloat) -> Option<TipResult> {
     if amt.is_none() || pct.is_none() {
         return None;
     }
 
     let amt = amt.unwrap();
-    let pct = pct.unwrap();
+    let pct = pct.unwrap() / 100f64;
 
     Some(TipResult {
         amt: amt.clone(),
         tip: &amt * &pct,
     })
-}
-
-fn ratio(input: &str) -> ORatio {
-    match input.parse::<f32>() {
-        Ok(f) => Ratio::from_float(f),
-        _ => None,
-    }
 }
